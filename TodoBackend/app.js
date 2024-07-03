15a2d62549ab0a9a57aaaa279742627f820ceb9c
sharonrajkondapaturi <sharoncheran@gmail.com>
2024-07-03 21:20:13 +0530

    first commit

---
+++ TodoBackend/app.js
@@ -0,0 +1,79 @@
+const express = require("express")
+const path = require("path")
+const {open} = require("sqlite")
+const sqlite3 = require("sqlite3").verbose()
+const bodyParser = require('body-parser')
+const app = express();
+const dbPath = path.join(__dirname,"todo.db")
+const cors = require("cors")
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
+let db = null;
+
+const initializeDbAndServer = async()=>{
+    try{
+        db = await open({
+            filename:dbPath,
+            driver:sqlite3.Database
+        });
+        app.listen(3000,()=>{
+            console.log(`Server is listening http://localhost:3000`);
+        })
+    }
+    catch(error){
+        console.log(`DB error : ${e.message}`)
+        process.exit(1)
+    }
+}
+
+const todoList = (eachTodo)=>{
+    return {
+        id:eachTodo.id,
+        todoName:eachTodo.todoName,
+        task1:eachTodo.task1,
+        task2:eachTodo.task2,
+        task3:eachTodo.task3,
+        priority:eachTodo.priority
+    }
+}
+
+
+initializeDbAndServer()
+
+app.get('/todo',async(request,response)=>{
+    const {search='',priority=''} = request.query
+    const getTodoQuery = `SELECT * from todo WHERE todoName LIKE "%${search}%" AND priority LIKE "%${priority}%";`
+    const todoArray = await db.all(getTodoQuery)
+    response.send(todoArray.map(eachTodo=> todoList(eachTodo)))
+})
+app.post('/updateTodo',async(request,response)=>{
+    const {todoName,task1,task2,task3,priority} = request.body
+    const todoQuery = `INSERT INTO todo(todoName,task1,task2,task3,priority) VALUES ("${todoName}","${task1}","${task2}","${task3}","${priority}");`
+    await db.run(todoQuery)
+    const getTodoQuery = `SELECT * from todo;`
+    const todoArray = await db.all(getTodoQuery)
+    response.send(todoArray.map(eachTodo=> todoList(eachTodo)))
+})
+
+app.put('/addTodo/:id',async(request,response)=>{
+    const {todoName,task1,task2,task3,priority} = request.body
+    const {id} = request.params
+    const todoQuery = `UPDATE todo SET todoName="${todoName}",task1="${task1}",task2="${task2}",task3="${task3}",priority="${priority}" 
+    WHERE id = ${id};`
+    await db.run(todoQuery)
+    const getTodoQuery = `SELECT * from todo;`
+    const todoArray = await db.all(getTodoQuery)
+    response.send(todoArray.map(eachTodo=> todoList(eachTodo)))
+})
+
+app.delete('/deleteTodo/:id',async(request,response)=>{
+    const {id} = request.params 
+    const todoQuery = `DELETE FROM todo WHERE id=${id};`
+    await db.run(todoQuery)
+    const getTodoQuery = `SELECT * FROM todo;`
+    const todoArray = await db.all(getTodoQuery)
+    response.send(todoArray.map(eachTodo=> todoList(eachTodo)))
+})
+
+module.exports = app
